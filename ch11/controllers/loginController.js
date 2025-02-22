9099f261b4b58938471d33515f2b4297446b108f
Abdurahman <eabdurahman8@gmail.com>
2025-02-22 11:53:36 +0300

    jwt autontication on logincontroller

---
+++ ch11/controllers/loginController.js
@@ -0,0 +1,52 @@
+const usersDb = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const bcrypt = require("bcrypt");
+const path = require("path");
+const fsPromise = require("fs").promises;
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config;
+const fsPromise = require('fs').promises;
+const path = require('path');
+
+
+const HandlerOldUser = async (req, res) => {
+  const user = req.body.user;
+  const pwd = req.body.pwd;
+
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ missage: "username and password are required!!!!" });
+     const foundUser = usersDb.users.find(person => (person.username === user));
+     if(!foundUser) return res.sendStatus(401);
+
+     const match = await bcrypt.compare(pwd,foundUser.password);
+      if(match){
+        // create JWTs
+        const accessToken = jwt.sign({'username':foundUser.username} ,process.env.ACCESS_TOKEN_SECRET,
+          {expiresIn:"30s"}
+        );
+        const refrashToken = jwt.sign({'username':foundUser.username} ,process.env.REFRASH_TOKEN_SECRET,
+          {expiresIn:"1d"}
+        );
+
+        const otherUsers = usersDb.users.filter(person => person.username !== foundUser.username);
+        const currentUser = {...foundUser,refreshToken};
+        usersDb.setUsers([...otherUsers,currentUser]);
+        await fsPromise.writeFile(
+          path.join(__dirname,"..","model","users.json"),
+          JSON.stringify(usersDb.users)
+        )
+        res.cookie('jwt',refrashToken,{httpOnly:true,maxAge: 24*60*60*1000});
+        res.json({accessToken});
+      }
+      else {res.sendStatus(401)}
+
+     }
+module.exports = {HandlerOldUser}
